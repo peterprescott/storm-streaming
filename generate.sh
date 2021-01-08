5b565e0a561d12d364861fbad038f375aff8eb39
Peter Prescott <prescott.peter@gmail.com>
2021-01-08 16:17:00 +0000

    Change app name

--- generate.sh
+++ generate.sh
@@ -1,1 +1,5 @@
-mvn -B archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4
+mvn -B archetype:generate \
+	-DgroupId=com.geekcap.storm \
+	-DartifactId=quickstart \
+	-DarchetypeArtifactId=maven-archetype-quickstart \
+	-DarchetypeVersion=1.4
