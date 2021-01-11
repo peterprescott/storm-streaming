60a749df9b8d06212536705ffa03e378414e0cbf
Peter Prescott <prescott.peter@gmail.com>
2021-01-11 11:34:06 +0000

    Adapt local example for cluster deployment

---
+++ cluster-example/copy_code.sh
@@ -0,0 +1,6 @@
+rm ../quickstart/src/main/java/piprescott/*
+rm -rf ../quickstart/src/test
+cp ./java/* ../quickstart/src/main/java/piprescott/
+cp working_pom.xml ../quickstart/pom.xml
+cp package.sh ../quickstart/package.sh
+
